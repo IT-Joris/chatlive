484450b847df27a6267c3f910dfbcc86da7949fc
IT-Joris <122261387+IT-Joris@users.noreply.github.com>
2023-07-03 17:49:22 +0200

    Ajout des onglets dans la nav mobile

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -143,7 +143,10 @@
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+                {{ __('Chat') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link href="{{ route('dashboard.file') }}" :active="request()->routeIs('dashboard.file')">
+                {{ __('File') }}
             </x-responsive-nav-link>
         </div>
 
